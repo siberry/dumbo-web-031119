23a6bbf25c0591bf307d4fdf41140f07c3d362c8
siberry <siberry226@gmail.com>
2019-04-01 12:50:57 -0400

    End of lecture

--- 07-inheritance/fish.rb
+++ 07-inheritance/fish.rb
@@ -1,23 +1,8 @@
 class Fish < Animal
 
   def initialize(name)
-<<<<<<< HEAD
     super #will call the initialize from the class it inherits
     @thing_this_animal_says = "Blub"
   end
 
 end
-=======
-    super
-    @thing_this_animal_says = "Blub"
-  end
-  # def speak
-  #   "Blub"
-  # end
-  
-end
-<<<<<<< HEAD
->>>>>>> d197237f9fbf6154a810d7e9272869cc0d9a460e
-=======
->>>>>>> ef0996dfb1471174b578bd1c774fba202e66d455
->>>>>>> serenas_notes
--- 10-rack-internet/app.rb
+++ 10-rack-internet/app.rb
@@ -4,8 +4,13 @@ class App
 
   def call(environment_hash)
 
-    status_code = 200
+    status_code = 418
     header = { "Content-type" =>  "text/html" }
+
+
+    if environment_hash["REQUEST-PATH"] == "/teapot"
+      body = ["I'm a little teapot"]
+
     # binding.pry
     body = ["Hello, world"]
 
